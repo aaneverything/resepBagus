1d6a3b7f915c089fbf73d87d2a86b63a9e192353
aanvi <aanvinanta5@gmail.com>
2025-07-01 01:12:42 +0700

    fix: update PostgreSQL environment variables in docker-compose.yml

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com', 'cookpad.com', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
     },
+    experimental: {
+        allowedDevOrigins: ["http://70.153.9.114"],
+    },
 };
 
 export default nextConfig;
