47b82a9e59d87bed6d509b9e911a5a943aa23191
aanvi <aanvinanta5@gmail.com>
2025-07-01 21:29:55 +0700

    fix: update error message display in FormRegister component

--- components/auth/form-register.jsx
+++ components/auth/form-register.jsx
@@ -29,7 +29,7 @@ const FormRegister = () => {
         <form action={formAction} className="space-y-5">
             {state?.message && (
                 <div className="p-4 mb-4 text-sm text-red-500 bg-red-100 rounded-md" role="alert" aria-live="polite" aria-atomic="true">
-                    <span className="font-medium">{state.errors}</span>
+                    <span className="font-medium">{state.message}</span>
                 </div>
             )}
 
